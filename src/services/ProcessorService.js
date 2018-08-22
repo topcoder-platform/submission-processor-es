e2246adcf10870b9bc742f6b500aa5bb2c78372b
Sharathkumar Anbu <sharathkumaranbu@gmail.com>
2018-08-22 13:43:46 +0530

    Combine review and reviewSummation with Submission

--- src/services/ProcessorService.js
+++ src/services/ProcessorService.js
@@ -2,12 +2,26 @@
  * Service for submission processor.
  */
 
+const _ = require('lodash')
 const Joi = require('joi')
 const logger = require('../common/logger')
 const helper = require('../common/helper')
 const config = require('config')
 
 const client = helper.getESClient()
+
+/**
+ * Get elastic search data.
+ * @param {String} id the Elastic search data id
+ * @returns {Object} Data from Elastic search
+ */
+function * getESData (id) {
+  return yield client.getSource({
+    index: config.get('esConfig.ES_INDEX'),
+    type: config.get('esConfig.ES_TYPE'),
+    id
+  })
+}
 
 /**
  * Create message in Elasticsearch.
@@ -20,6 +34,35 @@ function * create (message) {
     id: message.payload.id,
     body: message.payload
   })
+
+  // Add review / reviewSummation to submission
+  if (message.payload.resource === 'review') {
+    const submission = yield getESData(message.payload.submissionId)
+    let reviewArr = []
+    reviewArr.push(_.omit(message.payload, ['resource']))
+    if (submission.review) {
+      reviewArr = reviewArr.concat(submission.review)
+    }
+    yield client.update({
+      index: config.get('esConfig.ES_INDEX'),
+      type: config.get('esConfig.ES_TYPE'),
+      id: message.payload.submissionId,
+      body: { doc: {review: reviewArr} }
+    })
+  } else if (message.payload.resource === 'reviewSummation') {
+    const submission = yield getESData(message.payload.submissionId)
+    let reviewSummationArr = []
+    reviewSummationArr.push(_.omit(message.payload, ['resource']))
+    if (submission.reviewSummationArr) {
+      reviewSummationArr = reviewSummationArr.concat(submission.review)
+    }
+    yield client.update({
+      index: config.get('esConfig.ES_INDEX'),
+      type: config.get('esConfig.ES_TYPE'),
+      id: message.payload.submissionId,
+      body: { doc: {reviewSummation: reviewSummationArr} }
+    })
+  }
 }
 
 create.schema = {
@@ -46,6 +89,34 @@ function * update (message) {
     id: message.payload.id,
     body: { doc: message.payload }
   })
+
+  if (message.payload.resource === 'review') {
+    const review = yield getESData(message.payload.id)
+    const submission = yield getESData(review.submissionId)
+    const reviewToBeUpdated = _.filter(submission.review, {id: message.payload.id})[0]
+    _.extend(reviewToBeUpdated, _.omit(message.payload, ['resource']))
+    _.remove(submission.review, {id: message.payload.id})
+    submission.review.push(reviewToBeUpdated)
+    yield client.update({
+      index: config.get('esConfig.ES_INDEX'),
+      type: config.get('esConfig.ES_TYPE'),
+      id: submission.id,
+      body: { doc: {review: submission.review} }
+    })
+  } else if (message.payload.resource === 'reviewSummation') {
+    const reviewSummation = yield getESData(message.payload.id)
+    const submission = yield getESData(reviewSummation.submissionId)
+    const reviewSummationToBeUpdated = _.filter(submission.reviewSummation, {id: message.payload.id})[0]
+    _.extend(reviewSummationToBeUpdated, _.omit(message.payload, ['resource']))
+    _.remove(submission.reviewSummation, {id: message.payload.id})
+    submission.reviewSummation.push(reviewSummationToBeUpdated)
+    yield client.update({
+      index: config.get('esConfig.ES_INDEX'),
+      type: config.get('esConfig.ES_TYPE'),
+      id: submission.id,
+      body: { doc: {reviewSummation: submission.reviewSummation} }
+    })
+  }
 }
 
 update.schema = create.schema
@@ -55,6 +126,29 @@ update.schema = create.schema
  * @param {Object} message the message
  */
 function * remove (message) {
+  // Remove review / reviewSummation from submission data
+  if (message.payload.resource === 'review') {
+    const review = yield getESData(message.payload.id)
+    const submission = yield getESData(review.submissionId)
+    _.remove(submission.review, {id: message.payload.id})
+    yield client.update({
+      index: config.get('esConfig.ES_INDEX'),
+      type: config.get('esConfig.ES_TYPE'),
+      id: submission.id,
+      body: { doc: {review: submission.review} }
+    })
+  } else if (message.payload.resource === 'reviewSummation') {
+    const reviewSummation = yield getESData(message.payload.id)
+    const submission = yield getESData(reviewSummation.submissionId)
+    _.remove(submission.reviewSummation, {id: message.payload.id})
+    yield client.update({
+      index: config.get('esConfig.ES_INDEX'),
+      type: config.get('esConfig.ES_TYPE'),
+      id: submission.id,
+      body: { doc: {reviewSummation: submission.reviewSummation} }
+    })
+  }
+
   yield client.delete({
     index: config.get('esConfig.ES_INDEX'),
     type: config.get('esConfig.ES_TYPE'),
